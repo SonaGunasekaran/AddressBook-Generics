1118b3418573fa9582efa66366f258207386b316
[Sona] <[sona16061999@gmail.com]>
2021-07-22 23:35:55 +0530

    [Sona] Retrieve top 2 records from the

--- LambaExpression/Program.cs
+++ LambaExpression/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LambaExpression
 {
@@ -9,6 +10,7 @@ namespace LambaExpression
         {
             List<Person> list = new List<Person>();
             PersonDetails(list);
+            Retrieve(list);
         }
         public static void PersonDetails(List<Person> list)
         {
@@ -16,12 +18,25 @@ namespace LambaExpression
             list.Add(new Person() { SSN = 2, Name = "Daemon", Age = 14, Address = "Seattle", });
             list.Add(new Person() { SSN = 3, Name = "Chandler", Age = 18, Address = "California", });
             list.Add(new Person() { SSN = 4, Name = "Monica", Age = 65, Address = "Norway", });
-
+            //List the details of the person
             foreach (Person person in list)
             {
                 Console.WriteLine("SSN = {0}\tName={1}\tAddress = {2}\tAge={3}", person.SSN, person.Name, person.Address, person.Age);
             }
         }
+        //retrieve the top two ages in the list
+        public static void Retrieve(List<Person> list)
+        {
+            Console.WriteLine(" ");
+            //Lamba expression to retrieve the ages
+            List<Person> result = list.FindAll(p => p.Age < 60).OrderBy(x => x.Age).Take(2).ToList();
+
+            foreach (Person person in result)
+            {
+                Console.WriteLine("SSN = {0}\tName={1}\tAddress = {2}\tAge={3}", person.SSN, person.Name, person.Address, person.Age);
+            }
+        }
+
 
 
     }
