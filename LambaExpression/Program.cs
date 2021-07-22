aaf71e02b6b69fefbcd48da4a7b56ac1411522ba
[Sona] <[sona16061999@gmail.com]>
2021-07-22 22:36:27 +0530

    Initailised withnWelcome message

---
+++ LambaExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lamba Expression!");
+        }
+    }
+}
