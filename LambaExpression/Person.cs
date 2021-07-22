12597e390457abdd3ef5227122b0ea65ad540a8a
[Sona] <[sona16061999@gmail.com]>
2021-07-22 23:24:30 +0530

    [Sona] Adding Person Details into the list

---
+++ LambaExpression/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambaExpression
+{
+    class Person
+    {
+        public int SSN { get; set; }
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+    }
+}
+
--- LambaExpression/Program.cs
+++ LambaExpression/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LambaExpression
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Lamba Expression!");
+            List<Person> list = new List<Person>();
+            PersonDetails(list);
         }
+        public static void PersonDetails(List<Person> list)
+        {
+            list.Add(new Person() { SSN = 1, Name = "Kalus", Age = 72, Address = "India", });
+            list.Add(new Person() { SSN = 2, Name = "Daemon", Age = 14, Address = "Seattle", });
+            list.Add(new Person() { SSN = 3, Name = "Chandler", Age = 18, Address = "California", });
+            list.Add(new Person() { SSN = 4, Name = "Monica", Age = 65, Address = "Norway", });
+
+            foreach (Person person in list)
+            {
+                Console.WriteLine("SSN = {0}\tName={1}\tAddress = {2}\tAge={3}", person.SSN, person.Name, person.Address, person.Age);
+            }
+        }
+
+
     }
 }
